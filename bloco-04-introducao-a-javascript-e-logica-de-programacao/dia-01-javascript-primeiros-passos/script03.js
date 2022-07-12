97e362bef7ca195508c1aa5ee785eb66a0620f76
JenniferOSF <jenniferoliveira_bh@yahoo.com.br>
2022-07-12 19:36:09 -0300

    Exercicio 03

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script03.js
@@ -0,0 +1,11 @@
+const a = 15;
+const b = 10;
+const c = 5;
+
+if (a > b && a > c) {
+    console.log('O maior número é: ' + a + ' (a)');
+} else if ( b > a && b > c) {
+    console.log('O maior número é: ' + b + ' (b)');
+} else {
+    console.log('O maior número é: ' + c + ' (c)');
+}
