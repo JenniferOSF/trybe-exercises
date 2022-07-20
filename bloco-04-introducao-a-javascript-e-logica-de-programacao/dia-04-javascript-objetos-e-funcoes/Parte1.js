f1f040cf3d8278579a808dba5c39732012772178
JenniferOSF <jenniferoliveira_bh@yahoo.com.br>
2022-07-20 18:47:14 -0300

    Exercicio 08 - Parte 01

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/Parte1.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/Parte1.js
@@ -83,6 +83,28 @@
 
 // Exercicio 07 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações:
 
+//let leitor = {
+  //nome: 'Julia',
+  //sobrenome: 'Pessoa',
+  //idade: 21,
+  //livrosFavoritos: [
+    //{
+      //titulo: 'O Pior Dia de Todos',
+      //autor: 'Daniela Kopsch',
+      //editora: 'Tordesilhas',
+    //},
+  //],
+//};
+//leitor.livrosFavoritos.push (
+  //{
+    //titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    //autor: 'JK Rowling',
+    //editor: 'Rocco',
+  //}
+//)
+
+// Exercicio 08 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
 let leitor = {
   nome: 'Julia',
   sobrenome: 'Pessoa',
@@ -102,3 +124,5 @@ leitor.livrosFavoritos.push (
     editor: 'Rocco',
   }
 )
+
+console.log('"' + leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos".');
