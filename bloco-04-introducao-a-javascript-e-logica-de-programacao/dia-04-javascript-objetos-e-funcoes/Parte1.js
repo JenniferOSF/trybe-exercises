0b83b42826486b176a547ceb44e251ba724af39b
JenniferOSF <jenniferoliveira_bh@yahoo.com.br>
2022-07-20 16:00:21 -0300

    Exercicio 03 - Parte 01

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/Parte1.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/Parte1.js
@@ -11,12 +11,23 @@
 
 // Exercicio 02 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim',
-  };
+  //let info = {
+  //  personagem: 'Margarida',
+  //  origem: 'Pato Donald',
+  //  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  //  recorrente: 'Sim',
+  //  };
 
-  console.log(info);
-  
+  //console.log(info);
+
+// Exercicio 03 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
+for (let key in info) {
+    console.log(key);
+}
