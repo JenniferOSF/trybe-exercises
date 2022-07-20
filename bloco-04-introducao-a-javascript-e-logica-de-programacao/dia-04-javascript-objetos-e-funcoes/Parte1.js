8e68abb4733110a57baac5fd5c4303669bbdee34
JenniferOSF <jenniferoliveira_bh@yahoo.com.br>
2022-07-20 18:33:57 -0300

    Exercicio 06 - parte 01

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/Parte1.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/Parte1.js
@@ -47,21 +47,36 @@
 
 // Exercicio 05 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim',
-};
+//let info = {
+    //personagem: 'Margarida',
+    //origem: 'Pato Donald',
+    //nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    //recorrente: 'Sim',
+//};
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
-}
+//let info2 = {
+    //personagem: 'Tio Patinhas',
+    //origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    //nota: 'O último MacPatinhas',
+    //recorrente: 'Sim',
+//}
     
-for (let key in info) {
-    console.log(info[key] + ' e ' + info2[key]);
-}
+//for (let key in info) {
+    //console.log(info[key] + ' e ' + info2[key]);
+//}
 
+// Exercicio 06 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '".');
