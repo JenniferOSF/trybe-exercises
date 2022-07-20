48d99704b29e1dee5d19127442c80a9d8f65d38f
JenniferOSF <jenniferoliveira_bh@yahoo.com.br>
2022-07-20 15:39:08 -0300

    Exercicio 01 - parte 01

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/Parte1.js
@@ -0,0 +1,10 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  //Exercicio 01
+for (let personagem in info) {
+    console.log('Bem-vinda, ' + info(personagem));
+}
