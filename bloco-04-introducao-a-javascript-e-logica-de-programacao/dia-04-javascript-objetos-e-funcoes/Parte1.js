30cc7943b94b9d8427e4b3a06c1e3db605e65b78
JenniferOSF <jenniferoliveira_bh@yahoo.com.br>
2022-07-20 16:19:17 -0300

    Exercicio 05 - Parte 01

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/Parte1.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/Parte1.js
@@ -34,13 +34,34 @@
 
 // Exercicio 04 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
 
+//    let info = {
+//        personagem: 'Margarida',
+//        origem: 'Pato Donald',
+//        nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//        recorrente: 'Sim',
+//    };
+//    
+//    for (let key in info) {
+//        console.log(info[key]);
+//    }
+
+// Exercicio 05 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim',
-  };
-  
-  for (let key in info) {
-      console.log(info[key]);
-  }
+};
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+}
+    
+for (let key in info) {
+    console.log(info[key] + ' e ' + info2[key]);
+}
+
