5aa2681846b46a0d1b767f989989bec853802395
JenniferOSF <jenniferoliveira_bh@yahoo.com.br>
2022-07-20 18:38:53 -0300

    Exercicio 07 - Parte 01

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/Parte1.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/Parte1.js
@@ -67,16 +67,38 @@
 
 // Exercicio 06 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
 
+//let leitor = {
+    //nome: 'Julia',
+    //sobrenome: 'Pessoa',
+    //idade: 21,
+    //livrosFavoritos: [
+      //{
+        //titulo: 'O Pior Dia de Todos',
+        //autor: 'Daniela Kopsch',
+        //editora: 'Tordesilhas',
+      //},
+    //],
+  //};
+//console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '".');
+
+// Exercicio 07 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações:
+
 let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-      {
-        titulo: 'O Pior Dia de Todos',
-        autor: 'Daniela Kopsch',
-        editora: 'Tordesilhas',
-      },
-    ],
-  };
-console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '".');
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+leitor.livrosFavoritos.push (
+  {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editor: 'Rocco',
+  }
+)
