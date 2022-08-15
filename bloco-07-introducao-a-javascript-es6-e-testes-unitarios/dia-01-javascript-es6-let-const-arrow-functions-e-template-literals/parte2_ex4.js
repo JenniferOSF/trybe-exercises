91233535980a36050939a130371808f229c8a75b
JenniferOSF <jenniferoliveira_bh@yahoo.com.br>
2022-08-15 13:07:35 -0300

    Exercicio 4.2 - parte 2

--- bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/parte2_ex4.js
+++ bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/parte2_ex4.js
@@ -3,9 +3,20 @@ const substituaX = (nome) => {
     const fraseArray = frase.split(' ');
     for(index = 0; index < fraseArray.length; index += 1) {
         if (fraseArray[index] === 'x') {
-            fraseArrau[index] = nome;
+            fraseArray[index] = nome;
         }
     }
     return fraseArray.join(' ');
 };
-console.log(substituaX('Bebeto'));
+//console.log(substituaX('Bebeto'));
+
+const minhasSkills = (param1) => {
+    const skills = ['HTML', 'CSS', 'JavaScript'];
+    let result = `${param1} Minhas três principais habilidades são:`
+    for (let index = 0; index < skills.length; index += 1) {
+        result = `${result} - ${skills[index]}`;
+    }
+    return result;
+};
+
+console.log(minhasSkills(substituaX('Bebeto')));
