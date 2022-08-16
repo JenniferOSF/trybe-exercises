40670d98e2d10d16af8cbd033813b6d0a1dfc86d
JenniferOSF <jenniferoliveira_bh@yahoo.com.br>
2022-08-16 12:53:11 -0300

    Exercicio 01

---
+++ bloco-8-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -0,0 +1,15 @@
+const employeesGenerate = (fullName) => {
+    const email = fullName.toLowerCase().split(' ').join('_');
+    return { fullName, email: `${fullName}@trybe.com`};
+}
+
+const newEmployees = (callback) => {
+    const employees = {
+      id1: callback('Pedro Guerra'), 
+      id2: callback('Luiza Drumond'),
+      id3: callback('Carla Paiva'),
+    };
+    return employees;
+  };
+
+  console.log(newEmployees(employeesGenerate));
