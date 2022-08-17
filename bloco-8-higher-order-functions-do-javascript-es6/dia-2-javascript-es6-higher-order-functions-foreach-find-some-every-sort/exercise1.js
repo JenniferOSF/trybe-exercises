e0801750298843c54e8e4718225105c6dda7fb43
JenniferOSF <jenniferoliveira_bh@yahoo.com.br>
2022-08-17 17:06:06 -0300

    exercicio 4 - aplicando sort

--- bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
+++ bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
@@ -1,102 +1,52 @@
-const books = [
+//exercicio 4
+const expectedResult = [
     {
       id: 1,
       name: 'As Crônicas de Gelo e Fogo',
       genre: 'Fantasia',
-      author: {
-        name: 'George R. R. Martin',
-        birthYear: 1948,
-      },
+      author: { name: 'George R. R. Martin', birthYear: 1948 },
       releaseYear: 1991,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: { name: 'Stephen King', birthYear: 1947 },
+      releaseYear: 1986,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: { name: 'Frank Herbert', birthYear: 1920 },
+      releaseYear: 1965,
     },
     {
       id: 2,
       name: 'O Senhor dos Anéis',
       genre: 'Fantasia',
-      author: {
-        name: 'J. R. R. Tolkien',
-        birthYear: 1892,
-      },
+      author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
       releaseYear: 1954,
     },
     {
       id: 3,
       name: 'Fundação',
       genre: 'Ficção Científica',
-      author: {
-        name: 'Isaac Asimov',
-        birthYear: 1920,
-      },
+      author: { name: 'Isaac Asimov', birthYear: 1920 },
       releaseYear: 1951,
     },
-    {
-      id: 4,
-      name: 'Duna',
-      genre: 'Ficção Científica',
-      author: {
-        name: 'Frank Herbert',
-        birthYear: 1920,
-      },
-      releaseYear: 1965,
-    },
-    {
-      id: 5,
-      name: 'A Coisa',
-      genre: 'Terror',
-      author: {
-        name: 'Stephen King',
-        birthYear: 1947,
-      },
-      releaseYear: 1986,
-    },
+
     {
       id: 6,
       name: 'O Chamado de Cthulhu',
       genre: 'Terror',
-      author: {
-        name: 'H. P. Lovecraft',
-        birthYear: 1890,
-      },
+      author: { name: 'H. P. Lovecraft', birthYear: 1890 },
       releaseYear: 1928,
     },
-];
-
-const expectedResult = {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948,
-    },
-    releaseYear: 1991,
-};
+  ];
   
-// aplicando o find
-
-function authorBornIn1947() {
-    return books.find((book) => book.author.birthYear === 1947).author.name;
-}
-console.log(authorBornIn1947(books));
-
-// aplicando o forEach
-
-function smallerName() {
-    let nameBook;
-    books.forEach ((book) => {
-        if(!nameBook || book.name.length < nameBook.length){
-            nameBook = book.name;
-        }
-    })
-    
-    return nameBook;
-}
-console.log(smallerName());
-
-// exercicio 3
-
-function getNamedBook() {
-    return books.find((book) => book.name.length === 26);
-}
-
-console.log(getNamedBook());
+  function booksOrderedByReleaseYearDesc() {
+    return expectedResult.sort((bookA, bookB) => bookB.releaseYear - bookA.releaseYear);
+  }
+  
+  console.log(booksOrderedByReleaseYearDesc());
